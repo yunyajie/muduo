8ed5adab0eb2e43c50f5c647e35623637eb1b1db
yajie yun <yajieyun@mail.ustc.edu.cn>
2024-01-17 21:37:54 +0800

    注释

--- examples/asio/chat/codec.h
+++ examples/asio/chat/codec.h
@@ -22,27 +22,27 @@ class LengthHeaderCodec : muduo::noncopyable
                  muduo::net::Buffer* buf,
                  muduo::Timestamp receiveTime)
   {
-    while (buf->readableBytes() >= kHeaderLen) // kHeaderLen == 4
+    while (buf->readableBytes() >= kHeaderLen) // kHeaderLen == 4  只要可能出现一条消息就尝试解码读取消息
     {
       // FIXME: use Buffer::peekInt32()
       const void* data = buf->peek();
-      int32_t be32 = *static_cast<const int32_t*>(data); // SIGBUS
-      const int32_t len = muduo::net::sockets::networkToHost32(be32);
+      int32_t be32 = *static_cast<const int32_t*>(data); // SIGBUS 先获取消息头部的长度字段
+      const int32_t len = muduo::net::sockets::networkToHost32(be32); //转换为主机字节序
       if (len > 65536 || len < 0)
       {
         LOG_ERROR << "Invalid length " << len;
         conn->shutdown();  // FIXME: disable reading
         break;
       }
-      else if (buf->readableBytes() >= len + kHeaderLen)
+      else if (buf->readableBytes() >= len + kHeaderLen)  //可以获取一条完整的消息
       {
-        buf->retrieve(kHeaderLen);
-        muduo::string message(buf->peek(), len);
-        messageCallback_(conn, message, receiveTime);
-        buf->retrieve(len);
+        buf->retrieve(kHeaderLen);    //retrieve 长度字段的 4 个字节
+        muduo::string message(buf->peek(), len);  //读取消息的内容，len 个字节
+        messageCallback_(conn, message, receiveTime);   //消息读取到后，执行用户注册的消息到来时需要执行的回调函数
+        buf->retrieve(len);   //retrieve 消息的内容
       }
       else
-      {
+      { //没有收到一条完整的消息，退出循环
         break;
       }
     }
